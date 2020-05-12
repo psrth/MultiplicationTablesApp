8c6841e09105560b3ca21bde5259e8f4440d2b16
Parth Sharma <parthsharma151@gmail.com>
2020-05-12 21:13:31 +0530

    setup multiplication table calculator and scrub bar

--- app/src/main/java/com/example/multiplicationtables/MainActivity.java
+++ app/src/main/java/com/example/multiplicationtables/MainActivity.java
@@ -3,12 +3,71 @@ package com.example.multiplicationtables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    ListView myListView;
+    ArrayList<String> multTable;
+
+    public void updateListView (int i)
+    {
+
+
+        multTable.add(Integer.toString(i*1));
+        multTable.add(Integer.toString(i*2));
+        multTable.add(Integer.toString(i*3));
+        multTable.add(Integer.toString(i*4));
+        multTable.add(Integer.toString(i*5));
+        multTable.add(Integer.toString(i*6));
+        multTable.add(Integer.toString(i*7));
+        multTable.add(Integer.toString(i*8));
+        multTable.add(Integer.toString(i*9));
+        multTable.add(Integer.toString(i*10));
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, multTable);
+        myListView.setAdapter(arrayAdapter);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar numberSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        numberSeekBar.setMax(10);
+
+        myListView = (ListView) findViewById(R.id.listView);
+        multTable = new ArrayList<String>();
+
+        numberSeekBar.setProgress(2);
+        updateListView(2);
+
+
+        numberSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                multTable.clear();
+                updateListView(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
     }
 }
